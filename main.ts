9347518eaba834205d058d6c2c021798e0da1e50
AlexanderKronick <92746548+AlexanderKronick@users.noreply.github.com>
2021-10-19 12:14:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     snake.turn(Direction.Right, -90)
-    snakeb.turn(Direction.Right, -90)
 })
 input.onButtonPressed(Button.B, function () {
     snake.turn(Direction.Right, 90)
-    snakeb.turn(Direction.Right, 90)
 })
 let snakeb: game.LedSprite = null
 let snake: game.LedSprite = null
-snake = game.createSprite(1, 2)
+snake = game.createSprite(0, 2)
 let fruit = game.createSprite(randint(0, 4), randint(0, 4))
-snakeb = game.createSprite(0, 2)
 basic.forever(function () {
     while (true) {
         snake.move(1)
-        snakeb.move(1)
         basic.pause(1000)
-    }
-})
-basic.forever(function () {
-    if (snake.isTouching(snakeb)) {
-        game.gameOver()
     }
 })
 basic.forever(function () {
     if (snake.isTouching(fruit)) {
         fruit.set(LedSpriteProperty.X, randint(0, 4))
         fruit.set(LedSpriteProperty.Y, randint(0, 4))
+        snakeb = game.createSprite(2, 2)
     }
 })
